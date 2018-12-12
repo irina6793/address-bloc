72272ffe4e2f05503edf3962fcb6d5e93f2b868c
Irina Sverzhanovskaya <irina6793@yahoo.com>
2018-12-11 22:55:20 -0500

    first

--- spec/contact_controller_spec.js
+++ spec/contact_controller_spec.js
@@ -35,24 +35,22 @@ describe("ContactController", () => {
         .then((contacts) => {
           expect(contacts.length).toBe(0);
             done();
+          }
+        };
+    it("should return an array of contacts when contacts are available", (done) => {
+       this.book.addContact("Alice", "001-101-1010", "alice@example.com")
+          .then(() => {
+            this.book.getContacts()
+             .then((contacts) => {
+               expect(contacts.length).toBe(1);
+               done();
+           });
+          });
   })
       .catch((err) => {
          console.log(err);
           done();
     });
-
-  it("should return an array of contacts when contacts are available", (done) => {
-     this.book.addContact("Alice", "001-101-1010", "alice@example.com")
-     .then(() => {
-     this.book.getContacts()
-     .then((contacts) => {
-      expect(contacts.length).toBe(1);
-     done();
- });
-});
-   .catch((err) => {
-     console.log(err);
-     done();
 });
   it("should return null when contact is not found", (done) => {
     this.book.addContact(...zelda)
