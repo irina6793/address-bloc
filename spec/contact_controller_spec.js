f0bd947ce9487014adeb0d995e3414d29b5adcaa
Irina Sverzhanovskaya <irina6793@yahoo.com>
2018-12-14 13:24:47 -0500

    first

--- spec/contact_controller_spec.js
+++ spec/contact_controller_spec.js
@@ -140,22 +140,22 @@ beforeEach((done) => {
 });
 //#10
 describe("search methods", () => {
-
     const zelda = ["Zelda Smith", "000-100-111", "zelda@nintendo.com"];
     const snake = ["Solid Snake", "100-100-100", "snake@konami.com"];
     const magus = ["Magus Johnson", "101-010-101", "magus@squaresoft.com"];
     const alloy = ["Alloy Rodriguez", "111-111-111", "allow@guerrilla-games.com"];
-  });
-  beforeEach((done) => {
-     book = new ContactController();
-//#1
-    sequelize.sync({force: true}).then((res) => {
-      done();
-   })
-    .catch((err) => {
-      done();
+    beforeEach((done) => {
+       book = new ContactController();
+  //#1
+      sequelize.sync({force: true}).then((res) => {
+        done();
+     })
+      .catch((err) => {
+        done();
+      });
     });
   });
+
 //#11
 describe("#delete()", () => {
   beforeEach((done) => {
