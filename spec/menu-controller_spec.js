380700a22a2fd1e992a5523941cbd396e3e9cdf4
Irina <irina6793@yahoo.com>
2019-06-09 14:00:01 -0400

    update

--- spec/menu-controller_spec.js
+++ spec/menu-controller_spec.js
@@ -1,24 +1,13 @@
 const MenuController = require("../controllers/MenuController");
-// #1
- describe("MenuController", () => {
-   beforeEach(() => {
-      this.menu = new MenuController();
-    });
 
- describe("#remindMe()", () => {
-
-// #2
-
-
+describe("MenuController", () => {
+  beforeEach(() => {
+    this.menu = new MenuController();
   });
 
-
-
-
-
-
-      it("Learning is a life-long pursuit.", () => {
-        expect(this.menu.remindMe()).toBe("Learning is a life-long pursuit.")
-      });
- });
- 
+  describe("#remindMe()", () => {
+    it("Learning is a life-long pursuit.", () => {
+      expect(this.menu.remindMe());
+    });
+  });
+});
